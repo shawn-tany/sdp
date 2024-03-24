ca252bc627e1ec3bbf06d43d009230a4e0747bfe
txy <txy@Ubuntu22.04.4>
2024-03-24 23:25:11 +0800

    2024/03/24 23:25

--- components/transmission/base/tcp/server/tcp_server.h
+++ components/transmission/base/tcp/server/tcp_server.h
@@ -1,1 +1,30 @@
+#ifndef TCP_SERVER
+#define TCP_SERVER
 
+#include "common.h"
+
+#define TCP_ETHDEV_LEN 32
+#define TCP_IPADDR_LEN 32
+
+typedef int (*trans_callback_func)(void *, int);
+
+typedef struct 
+{
+    UINT16_T port;
+    char ip[TCP_ETHDEV_LEN];
+    char ethdev[TCP_IPADDR_LEN];
+} tcp_server_desc_t;
+
+typedef struct 
+{
+    trans_callback_func recv_callback;
+    trans_callback_func send_callback;
+} tcp_server_func_t;
+
+typedef struct 
+{
+    tcp_server_desc_t desc;
+    tcp_server_func_t func;
+} tcp_server_t;
+
+#endif

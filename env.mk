21198caec1544e22b4aadc2994447759e5c046f4
root <root@localhost.localdomain>
2024-04-07 21:19:48 +0800

    change Makefile

--- env.mk
+++ env.mk
@@ -7,22 +7,29 @@ compile_check :
 	@if [ ! -d "$(BUILD_DIR)/obj" ]; then mkdir $(BUILD_DIR)/obj; fi
 	@if [ ! -d "$(BUILD_DIR)/bin" ]; then mkdir $(BUILD_DIR)/bin; fi
 
+build_lib_check :
+	@if [ ! -d "$(CREATE_LIB_DIR)" ]; then mkdir $(CREATE_LIB_DIR); fi
+
 install_check :
 	@echo "install check"
 
-compile :
-	@echo "  ## build $(APP)"
-	@for src in $(SRCS);            \
-	do                              \
-	    ($(CC) $(CFLAGS) $(LIBS) $$src -c) && (echo "  [SUCCESS] $$src") || ((echo "  [FAILED ] $$src" && (rm *.o -rf)) && (exit 1)); \
-	done;
-	@mv *.o $(BUILD_DIR)/obj
+OBJALL := $(patsubst %.c, %.o, $(SRCS))
+OBJOUT := $(patsubst %.c, %.o, $(filter-out $(OUTSRCS), $(SRCS)))
+
+compile : $(OBJALL)
 	@echo "  link to $(APP)"
-	@$(CC) $(BUILD_DIR)/obj/*.o -o $(BUILD_DIR)/bin/$(APP) $(CFLAGS) $(LIBS)
-	@rm -rf $(BUILD_DIR)/obj/*
+	@$(CC) $(OBJALL) -o $(BUILD_DIR)/bin/$(APP) $(CFLAGS) $(LINK_LIBS)
 	@echo "  ## build $(APP) success!"
 
+build_lib : $(OBJALL)
+	@echo "  ## build lib $(CREATE_LIB)..."
+	@$(CC) -fPIC -shared $(OBJOUT) -o $(CREATE_LIB_DIR)/$(CREATE_LIB).so
+	@$(AR) rcs $(CREATE_LIB_DIR)/$(CREATE_LIB).a $(OBJOUT)
+
+%.o: %.c
+	@($(CC) $(CFLAGS) -c -fPIC -o $@ $<) && ((echo "  [SUCCESS] $<") || ((echo "  [FAILED ] $<" && (rm *.o -rf)) && (exit 1)))
+ 
 clean_build :
 	@echo "  ## clean $(APP)..."
-	@rm -rf $(BUILD_DIR)
+	@rm -rf $(BUILD_DIR) $(OBJALL)
 	@echo "  ## clean $(APP) success!"

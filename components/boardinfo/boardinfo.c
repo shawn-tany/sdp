ab761120882262ade1130755cd8001dc9a7a94e8
root <root@localhost.localdomain>
2024-04-08 09:18:01 +0800

    2024/04/08 09:18

--- components/boardinfo/boardinfo.c
+++ components/boardinfo/boardinfo.c
@@ -230,6 +230,77 @@ static int bi_cpuinfo_int_get(int cpu_index, char *key, int *value)
     return ret;
 }
 
+static int bi_cpuinfo_u64_get(int cpu_index, char *key, SIZE_T *value)
+{
+    FILE *fp;
+    char *ptr = NULL;
+    char  line[256] = {0};    
+    int   num = 0;
+    int   ret = -1;
+
+    if (!value || !key)
+    {
+        return -1;
+    }
+    
+    fp = fopen(BI_CPU_INFO_FILE, "r");
+    if (!fp) 
+    {    
+        perror("fopen");
+        return -1;
+    }
+ 
+    while (fgets(line, sizeof(line), fp)) 
+    {
+        if (strncmp(line, "processor", 9)) 
+        {
+            continue;
+        }
+
+        ptr = strchr(line, ':');
+        if (!ptr)
+        {
+            break;
+        }
+
+        num = strtol(ptr + 1, NULL, 10);
+        
+        if (num != cpu_index) 
+        {
+            continue;
+        }
+        
+        while (fgets(line, sizeof(line), fp)) 
+        {
+            if ('\n' == line[0])
+            {
+                break;
+            }
+        
+            if (strncmp(line, key, strlen(key))) 
+            {
+                continue;
+            }
+
+            ptr = strchr(line, ':');
+            if (ptr)
+            {
+                ptr += 2;
+                *value = strtol(ptr, NULL, 0);
+                ret = 0;
+            }
+
+            break;
+        }
+
+        break;
+    }
+ 
+    fclose(fp);
+    
+    return ret;
+}
+
 static int cal_cpu_usagerate(CPU_STAT_T *newstat, CPU_STAT_T *oldstat, float *rate)  
 {
     if (!newstat || !oldstat || !rate)
@@ -378,6 +449,17 @@ static int bi_diskinfo_get(DISKS_INFO_T *dinfo)
     return 0;
 }
 
+/*
+ * 功能 : 单位转换(bit、byte、KB、MB、GB)
+ * 参数 : 
+ *      src : 转换前的值
+ *      src_unit ：转换前的单位
+ *      dst : 转换后的值<输出参数>
+ *      dst_unit : 转换后的单位<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_unit_convert(double src, UNIT_TYPE_T src_unit, double *dst, UNIT_TYPE_T *dst_unit)
 {
     if (!dst || !dst_unit)
@@ -413,7 +495,15 @@ int bi_unit_convert(double src, UNIT_TYPE_T src_unit, double *dst, UNIT_TYPE_T *
     return 0;
 }
 
-char *bi_unit_str(int unit)
+/* 
+ * 功能 : 返回单位字符串(bit、byte、KB、MB、GB)
+ * 参数 ：
+ *      unit : 单位类型(    UNIT_TYPE_BIT、UNIT_TYPE_BYTE、UNIT_TYPE_KB、UNIT_TYPE_MB、UNIT_TYPE_GB)
+ * 返回值 :
+ *      成功返回单位对应的字符串
+ *      失败返回"unkown"
+ */
+char *bi_unit_str(UNIT_TYPE_T unit)
 {
     return (UNIT_TYPE_BIT  == unit) ? "bit"  :
            (UNIT_TYPE_BYTE == unit) ? "byte" :
@@ -422,6 +512,14 @@ char *bi_unit_str(int unit)
            (UNIT_TYPE_GB   == unit) ? "GB"   : "unkown";
 }
 
+/*
+ * 功能 : 获取cpu核心总数
+ * 参数 :
+ *      cpu_num : cpu核心数量<输出参数>
+ * 返回 :
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_num_get(int *cpu_num)
 {
     if (!cpu_num)
@@ -439,6 +537,16 @@ int bi_cpu_num_get(int *cpu_num)
     return 0;
 }
 
+/* 
+ * 功能 : 获取cpu型号
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      cpu_name : cpu型号<输出参数>
+ *      cpu_name_size : cpu型号的最大长度
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_name_get(int cpu_index, char *cpu_name, int cpu_name_size)
 {
     if (!cpu_name)
@@ -449,6 +557,15 @@ int bi_cpu_name_get(int cpu_index, char *cpu_name, int cpu_name_size)
     return bi_cpuinfo_string_get(cpu_index, "model name", cpu_name, cpu_name_size);
 }
 
+/* 
+ * 功能 : 获取cpu频率
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      freq : cpu频率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_freq_get(int cpu_index, int *freq)
 {
     if (!freq)
@@ -459,16 +576,34 @@ int bi_cpu_freq_get(int cpu_index, int *freq)
     return bi_cpuinfo_int_get(cpu_index, "cpu MHz", freq);
 }
 
-int bi_cpu_cachesize_get(int cpu_index, int *size)
+/* 
+ * 功能 : 获取cpu频率
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      size : cpu缓存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_cpu_cachesize_get(int cpu_index, SIZE_T *size)
 {
     if (!size)
     {
         return -1;
     }
     
-    return bi_cpuinfo_int_get(cpu_index, "cache size", size);    
+    return bi_cpuinfo_u64_get(cpu_index, "cache size", size);    
 }
 
+/* 
+ * 功能 : 获取单个cpu核心使用率
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      rate : cpu占用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_usagerate_get(int cpu_index, float *rate)
 {
     FILE* fp = NULL;
@@ -532,6 +667,14 @@ int bi_cpu_usagerate_get(int cpu_index, float *rate)
     return ret;
 }
 
+/* 
+ * 功能 : 获取cpu总使用率
+ * 参数 :
+ *      rate : cpu占用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_total_usagerate_get(float *rate)
 {
     if (!rate)
@@ -542,7 +685,14 @@ int bi_cpu_total_usagerate_get(float *rate)
     return bi_cpu_usagerate_get(-1, rate);
 }
 
-
+/* 
+ * 功能 : 获取内存使用率
+ * 参数 :
+ *      rate : 内存使用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_mem_usagerate_get(float *rate)
 {
     if (!rate)
@@ -562,7 +712,15 @@ int bi_mem_usagerate_get(float *rate)
     return 0;
 }
 
-int bi_mem_totalsize_get(int *size)
+/* 
+ * 功能 : 获取内存总量
+ * 参数 :
+ *      size : 内存总量，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_totalsize_get(SIZE_T *size)
 {
     if (!size)
     {
@@ -581,7 +739,15 @@ int bi_mem_totalsize_get(int *size)
     return 0;
 }
 
-int bi_mem_avaliablesize_get(int *size)
+/* 
+ * 功能 : 获取可用物理内存大小
+ * 参数 :
+ *      size : 可用物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_avaliablesize_get(SIZE_T *size)
 {
     if (!size)
     {
@@ -600,7 +766,15 @@ int bi_mem_avaliablesize_get(int *size)
     return 0;
 }
 
-int bi_mem_freesize_get(int *size)
+/* 
+ * 功能 : 获取未被使用的物理内存大小
+ * 参数 :
+ *      size : 未被使用的物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_freesize_get(SIZE_T *size)
 {
     if (!size)
     {
@@ -619,7 +793,15 @@ int bi_mem_freesize_get(int *size)
     return 0;
 }
 
-int bi_mem_buffersize_get(int *size)
+/* 
+ * 功能 : 获取用于块设备缓冲区的物理内存大小
+ * 参数 :
+ *      size : 用于块设备缓冲区的物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_buffersize_get(SIZE_T *size)
 {
     if (!size)
     {
@@ -638,7 +820,15 @@ int bi_mem_buffersize_get(int *size)
     return 0;
 }
 
-int bi_mem_cachedsize_get(int *size)
+/* 
+ * 功能 : 获取用于用户空间普通文件页的物理内存大小
+ * 参数 :
+ *      size : 用于用户空间普通文件页的物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_cachedsize_get(SIZE_T *size)
 {
     if (!size)
     {
@@ -657,6 +847,14 @@ int bi_mem_cachedsize_get(int *size)
     return 0;
 }
 
+/* 
+ * 功能 : 获取磁盘数量
+ * 参数 :
+ *      disk_num : 磁盘数量<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_disk_num_get(int *disk_num)
 {
     if (!disk_num)
@@ -676,6 +874,16 @@ int bi_disk_num_get(int *disk_num)
     return 0;
 }
 
+/* 
+ * 功能 : 获取磁盘名称<系统下的命名>
+ * 参数 :
+ *      disk_index : 磁盘下标，0 ~ <disk_num - 1>
+ *      name : 系统下的磁盘名称<输出参数>
+ *      name_size : 磁盘名称的最大长度
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_disk_name_get(int disk_index, char *name, int name_size)
 {
     if (!name)
@@ -700,7 +908,16 @@ int bi_disk_name_get(int disk_index, char *name, int name_size)
     return 0;
 }
 
-int bi_disk_size_get(int disk_index, int *size)
+/* 
+ * 功能 : 获取磁盘容量
+ * 参数 :
+ *      disk_index : 磁盘下标，0 ~ <disk_num - 1>
+ *      size : 磁盘容量，单位bytes<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_disk_size_get(int disk_index, SIZE_T *size)
 {
     if (!size)
     {
@@ -724,7 +941,15 @@ int bi_disk_size_get(int disk_index, int *size)
     return 0;
 }
 
-
+/* 
+ * 功能 : 获取磁盘使用率
+ * 参数 :
+ *      disk_index : 磁盘下标，0 ~ <disk_num - 1>
+ *      rate : 磁盘使用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_disk_usagerate_get(int disk_index, float *rate)
 {
     if (!rate)
--- components/boardinfo/boardinfo.h
+++ components/boardinfo/boardinfo.h
@@ -10,43 +10,201 @@ typedef enum
     UNIT_TYPE_GB,
 } UNIT_TYPE_T;
 
-char *bi_unit_str(int unit);
+typedef unsigned long long SIZE_T;
 
+/* 
+ * 功能 : 返回单位字符串(bit、byte、KB、MB、GB)
+ * 参数 ：
+ *      unit : 单位类型(    UNIT_TYPE_BIT、UNIT_TYPE_BYTE、UNIT_TYPE_KB、UNIT_TYPE_MB、UNIT_TYPE_GB)
+ * 返回值 :
+ *      成功返回单位对应的字符串
+ *      失败返回"unkown"
+ */
+char *bi_unit_str(UNIT_TYPE_T unit);
+
+/*
+ * 功能 : 单位转换(bit、byte、KB、MB、GB)
+ * 参数 : 
+ *      src : 转换前的值
+ *      src_unit ：转换前的单位
+ *      dst : 转换后的值<输出参数>
+ *      dst_unit : 转换后的单位<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_unit_convert(double src, UNIT_TYPE_T src_unit, double *dst, UNIT_TYPE_T *dst_unit);
 
 /* cpu info */
+/*
+ * 功能 : 获取cpu核心总数
+ * 参数 :
+ *      cpu_num : cpu核心数量<输出参数>
+ * 返回 :
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_num_get(int *cpu_num);
 
+/* 
+ * 功能 : 获取cpu型号
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      cpu_name : cpu型号<输出参数>
+ *      cpu_name_size : cpu型号的最大长度
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_name_get(int cpu_index, char *cpu_name, int cpu_name_size);
 
+/* 
+ * 功能 : 获取cpu频率
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      freq : cpu频率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_freq_get(int cpu_index, int *freq);
 
-int bi_cpu_cachesize_get(int cpu_index, int *size);
+/* 
+ * 功能 : 获取cpu频率
+ * 参数 :
+ *      cpu_index : cpu核心下标
+ *      size : cpu缓存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_cpu_cachesize_get(int cpu_index, SIZE_T *size);
 
+/* 
+ * 功能 : 获取cpu使用率
+ * 参数 :
+ *      cpu_index : cpu核心下标，若下标为-1，则获取cpu总使用率；若下标处于 0 ~ <cpu_num -1> 范围内，则获取单个核心的占用率
+ *      rate : cpu占用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_usagerate_get(int cpu_index, float *rate);
 
+/* 
+ * 功能 : 获取cpu总使用率
+ * 参数 :
+ *      rate : cpu占用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_cpu_total_usagerate_get(float *rate);
 
 /* memory info */
+/* 
+ * 功能 : 获取内存使用率
+ * 参数 :
+ *      rate : 内存使用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_mem_usagerate_get(float *rate);
 
-int bi_mem_totalsize_get(int *size);
+/* 
+ * 功能 : 获取内存总量
+ * 参数 :
+ *      size : 内存总量，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_totalsize_get(SIZE_T *size);
 
-int bi_mem_avaliablesize_get(int *size);
+/* 
+ * 功能 : 获取可用物理内存大小
+ * 参数 :
+ *      size : 可用物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_avaliablesize_get(SIZE_T *size);
 
-int bi_mem_freesize_get(int *size);
+/* 
+ * 功能 : 获取未被使用的物理内存大小
+ * 参数 :
+ *      size : 未被使用的物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_freesize_get(SIZE_T *size);
 
-int bi_mem_buffersize_get(int *size);
+/* 
+ * 功能 : 获取用于块设备缓冲区的物理内存大小
+ * 参数 :
+ *      size : 用于块设备缓冲区的物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_buffersize_get(SIZE_T *size);
 
-int bi_mem_cachedsize_get(int *size);
+/* 
+ * 功能 : 获取用于用户空间普通文件页的物理内存大小
+ * 参数 :
+ *      size : 用于用户空间普通文件页的物理内存大小，单位KB<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_mem_cachedsize_get(SIZE_T *size);
 
 /* disk info */
+/* 
+ * 功能 : 获取磁盘数量
+ * 参数 :
+ *      disk_num : 磁盘数量<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_disk_num_get(int *disk_num);
 
+/* 
+ * 功能 : 获取磁盘名称<系统下的命名>
+ * 参数 :
+ *      disk_index : 磁盘下标，0 ~ <disk_num - 1>
+ *      name : 系统下的磁盘名称<输出参数>
+ *      name_size : 磁盘名称的最大长度
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_disk_name_get(int disk_index, char *name, int name_size);
 
-int bi_disk_size_get(int disk_index, int *size);
+/* 
+ * 功能 : 获取磁盘容量
+ * 参数 :
+ *      disk_index : 磁盘下标，0 ~ <disk_num - 1>
+ *      size : 磁盘容量，单位bytes<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
+int bi_disk_size_get(int disk_index, SIZE_T *size);
 
+/* 
+ * 功能 : 获取磁盘使用率
+ * 参数 :
+ *      disk_index : 磁盘下标，0 ~ <disk_num - 1>
+ *      rate : 磁盘使用率<输出参数>
+ * 返回 : 
+ *      成功返回0
+ *      失败返回-1
+ */
 int bi_disk_usagerate_get(int disk_index, float *rate);
 
 #endif
